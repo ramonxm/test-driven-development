bb83483a7c1e45a4c8434162d289322b3ae87cbe
Ramon <ramon1234rxm@gmail.com>
2022-03-07 09:43:38 -0300

    test: adding lodash and update add method

--- jest-fundamentals/src/lib/Cart.js
+++ jest-fundamentals/src/lib/Cart.js
@@ -1,6 +1,13 @@
+import remove from "lodash/remove";
+import find from "lodash/find";
+
 export default class Cart {
   items = [];
   add(item) {
+    const itemToFind = { product: item.product };
+    if (find(this.items, itemToFind)) {
+      remove(this.items, itemToFind);
+    }
     this.items.push(item);
   }
   getTotal() {
--- jest-fundamentals/src/lib/Cart.spec.js
+++ jest-fundamentals/src/lib/Cart.spec.js
@@ -1,27 +1,42 @@
-import Cart from './Cart'
+import Cart from "./Cart";
 
-describe('Cart', () => {
+describe("Cart", () => {
   let cart;
+  let product;
 
   beforeEach(() => {
-    cart = new Cart()
+    cart = new Cart();
+    product = {
+      title: "Adidas running shoes - men",
+      price: 35388,
+    };
   });
 
-  it('should return 0 when getTotal() is executed in a newly created instance', () => {
-    expect(cart.getTotal()).toEqual(0)
+  it("should return 0 when getTotal() is executed in a newly created instance", () => {
+    expect(cart.getTotal()).toEqual(0);
   });
 
-  it('should multiply quantity and price receive the total amount', () => {
+  it("should multiply quantity and price receive the total amount", () => {
     const item = {
-      product: {
-        title: 'Adidas running shoes - men',
-        price: 35388,
-      },
+      product,
       quantity: 2,
     };
 
     cart.add(item);
 
     expect(cart.getTotal()).toEqual(70776);
-  })
+  });
+
+  it("should ensure no more than on product exists at a time", () => {
+    cart.add({
+      product,
+      quantity: 2,
+    });
+    cart.add({
+      product,
+      quantity: 1,
+    });
+
+    expect(cart.getTotal()).toEqual(35388);
+  });
 });
