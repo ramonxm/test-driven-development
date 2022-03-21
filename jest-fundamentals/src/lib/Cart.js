f5af47fa56347566ccfeeaa75b3d6e31e41e7eeb
Ramon <ramon1234rxm@gmail.com>
2022-03-21 14:46:34 -0300

    feat: implement discount by quantity

--- jest-fundamentals/src/lib/Cart.js
+++ jest-fundamentals/src/lib/Cart.js
@@ -2,6 +2,22 @@ import remove from "lodash/remove";
 import find from "lodash/find";
 import Money from "dinero.js";
 
+const calculatePercentageDiscount = (amount, item) => {
+  if (item.condition?.percentage && item.quantity > item.condition.minimum) {
+    return amount.percentage(item.condition.percentage);
+  }
+
+  return Money({ amount: 0 });
+};
+
+const calculateQuantityDiscount = (amount, item) => {
+  if (item.condition?.quantity && item.quantity > item.condition.quantity) {
+    return amount.percentage(50);
+  }
+
+  return Money({ amount: 0 });
+};
+
 Money.defaultCurrency = "BRL";
 Money.defaultPrecision = 2;
 export default class Cart {
@@ -23,12 +39,12 @@ export default class Cart {
       const amount = Money({ amount: item.quantity * item.product.price });
       let discount = Money({ amount: 0 });
 
-      if (
-        item.condition &&
-        item.condition.percentage &&
-        item.quantity > item.condition.minimum
-      ) {
-        discount = amount.percentage(item.condition.percentage);
+      if (item.condition?.percentage) {
+        discount = calculatePercentageDiscount(amount, item);
+      }
+
+      if (item.condition?.quantity) {
+        discount = calculateQuantityDiscount(amount, item);
       }
 
       return acc.add(amount).subtract(discount);
