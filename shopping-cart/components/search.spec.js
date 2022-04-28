74382a4fbd800baf31a593435d55324320be8a8d
Ramon <ramon1234rxm@gmail.com>
2022-04-28 08:56:39 -0300

    test: add suites tests to verify form manipulate

--- shopping-cart/components/search.spec.js
+++ shopping-cart/components/search.spec.js
@@ -1,9 +1,31 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Search from './search'
 
+const doSearch = jest.fn()
+
 describe('Search', () => {
-  it('should render', () => {
+  it('should render a form', () => {
     render(<Search />)
-    expect(screen.getByTestId('search')).toBeInTheDocument()
+    expect(screen.getByRole('form')).toBeInTheDocument()
+  })
+  it('should render an input type equals search', () => {
+    render(<Search doSearch={doSearch} />)
+    expect(screen.getByRole('searchbox')).toHaveProperty('type', 'search')
+  })
+  it('should call props.doSearch when the form is submitted', async () => {
+    render(<Search doSearch={doSearch} />)
+    const form = screen.getByRole('form');
+    await fireEvent.submit(form);
+    expect(doSearch).toHaveBeenCalledTimes(1);
+  })
+  it('should call props.doSearch with the user input', async () => {
+    render(<Search doSearch={doSearch} />)
+    const inputText = 'some text here';
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('searchbox');
+    await userEvent.type(input, inputText);
+    await fireEvent.submit(form);
+    expect(doSearch).toHaveBeenCalledWith(inputText);
   })
 })
