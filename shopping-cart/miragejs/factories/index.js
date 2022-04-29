06478ad37efc216025a4bcc20c1c38412ba00ac5
Ramon <ramon1234rxm@gmail.com>
2022-04-29 09:59:05 -0300

    chore: lint rule disable

--- shopping-cart/miragejs/factories/index.js
+++ shopping-cart/miragejs/factories/index.js
@@ -2,15 +2,16 @@
  * Mirage JS guide on Factories: https://miragejs.com/docs/data-layer/factories
  */
 
-import user from './user';
 import message from './message';
 import product from './product';
+import user from './user';
 
 /*
  * factories are contained in a single object, that's why we
  * destructure what's coming from users and the same should
  * be done for all future factories
  */
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   ...user,
   ...message,
