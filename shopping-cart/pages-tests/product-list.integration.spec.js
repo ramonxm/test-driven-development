635796b2715e93b0ccfe9637b5a86e2cc57f16a7
Ramon <ramon1234rxm@gmail.com>
2022-05-16 09:20:33 -0300

    test: finish tests integration

--- shopping-cart/pages-tests/product-list.integration.spec.js
+++ shopping-cart/pages-tests/product-list.integration.spec.js
@@ -1,6 +1,7 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import { makeServer } from '../miragejs/server';
 import ProductList from '../pages';
+import userEvent from '@testing-library/user-event';
 
 const renderProductList = () => render(<ProductList />);
 
@@ -31,25 +32,85 @@ describe('ProductCard', () => {
     renderProductList();
 
     await waitFor(() => {
-      expect(screen.getByTestId('no-products')).toBeInTheDocument();     
-    })
+      expect(screen.getByTestId('no-products')).toBeInTheDocument();
+    });
   });
   it('should display error message when promise is rejected', async () => {
     server.get('products', () => {
       throw new Error('Server is down');
-    })
+    });
     renderProductList();
 
     await waitFor(() => {
       expect(screen.getByTestId('server-error')).toBeInTheDocument();
       expect(screen.queryByTestId('no-products')).toBeNull();
       expect(screen.queryAllByTestId('product-card')).toHaveLength(0);
-
-    })
+    });
   });
   it('should filter the products list when a search is performed', async () => {
-    
+    const searchTerm = 'Relógio bonito';
+    server.createList('product', 2);
+
+    server.create('product', {
+      title: searchTerm,
+    });
+
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId('product-card')).toHaveLength(3);
+    });
+
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('searchbox');
+
+    await userEvent.type(input, searchTerm);
+    await fireEvent.submit(form);
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId('product-card')).toHaveLength(1);
+    });
   });
-  it.todo('should display the total quantity of products');
-  it.todo('should display product (singular) when there is only 1 product');
+  it('should display the total quantity of products', async () => {
+    server.createList('product', 10);
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByText(/10 Products/i)).toBeInTheDocument();
+    })
+
+  });
+  it('should display product (singular) when there is only 1 product', async () => {
+    server.create('product');
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByText(/1 Product$/i)).toBeInTheDocument();
+    })
+  });
+
+  it('should display proper quantity when list is filtered', async () => {
+    const searchTerm = 'Relógio bonito';
+    server.createList('product', 2);
+
+    server.create('product', {
+      title: searchTerm,
+    });
+
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByText(/3 Products/i)).toBeInTheDocument();
+    });
+
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('searchbox');
+
+    await userEvent.type(input, searchTerm);
+    await fireEvent.submit(form);
+
+    await waitFor(() => {
+      expect(screen.getByText(/1 Product$/i)).toBeInTheDocument();
+    });
+  });
 });
--- shopping-cart/pages/index.js
+++ shopping-cart/pages/index.js
@@ -1,15 +1,28 @@
+import { useEffect, useState } from 'react';
 import ProductCard from '../components/product-card';
 import Search from '../components/search';
 
 import { useFetchProducts } from '../hooks/use-fetch-products';
 export default function Home() {
   const { error, products } = useFetchProducts();
+  const [term, setTerm] = useState('');
+  const [localProducts, setLocalProducts] = useState([]);
+
+  useEffect(() => {
+    if (term === '') {
+      setLocalProducts(products);
+    } else {
+      setLocalProducts(
+        products.filter((product) => product.title.includes(term)),
+      );
+    }
+  }, [products, term]);
 
   const renderProductListOrMessage = () => {
-    if (products.length === 0 && !error) {
+    if (localProducts.length === 0 && !error) {
       return <h4 data-testid="no-products">No products</h4>;
     }
-    return products.map((product) => (
+    return localProducts.map((product) => (
       <ProductCard key={product.id} product={product} />
     ));
   };
@@ -19,14 +32,22 @@ export default function Home() {
       return null;
     }
     return <h4 data-testid="server-error">Server is down</h4>;
-  }
-  
+  };
+
+  const renderProductQuantity = () => {
+    return localProducts.length === 1
+      ? '1 Product'
+      : `${localProducts.length} Products`;
+  };
+
   return (
     <main data-testid="product-list" className="my-8">
-      <Search />
+      <Search doSearch={(term) => setTerm(term)} />
       <div className="container mx-auto px-6">
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
-        <span className="mt-3 text-sm text-gray-500">200+ Products</span>
+        <span className="mt-3 text-sm text-gray-500">
+          {renderProductQuantity()}
+        </span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
           {renderErrorMessage()}
           {renderProductListOrMessage()}
